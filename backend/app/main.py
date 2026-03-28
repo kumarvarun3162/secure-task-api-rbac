899a9eba1d9ba0f945584b08883997d9bdb9dd88
kumarvarun3162 <kumarvarun3162@gmail.com>
2026-03-28 23:02:46 +0530

    Backend Updated

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from .database import Base, engine
 from .routers import auth, tasks
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
@@ -10,6 +11,14 @@ app.include_router(auth.router)
 app.include_router(tasks.router)
 
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/")
 def root():
     return {"message": "API Running 🚀"}
